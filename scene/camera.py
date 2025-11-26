9712affc472446c3940956e687c71d648ac6a27d
DataKumpel <tmbenkenstein@gmail.com>
2025-11-26 23:19:06 +0100

    Added uniform buffer and bindgroup to camera

--- scene/camera.py
+++ scene/camera.py
@@ -1,7 +1,14 @@
 import glm
+import wgpu
+from graphics.renderer import Renderer
 
 class Camera:
-    def __init__(self, position: glm.vec3, aspect: float) -> None:
+    def __init__(self, renderer: Renderer, position: glm.vec3, aspect: float) -> None:
+        self.renderer = renderer
+        self.device = renderer.ctx.device
+        self.uniform_buffer = self._create_uniform_buffer()
+        self.bind_group = self._create_bind_group()
+
         self.position = position
         self.front = glm.vec3(0, 0, -1)
         self.up = glm.vec3(0, 1, 0)
@@ -29,3 +36,26 @@ class Camera:
         front.y = glm.sin(glm.radians(self.pitch))
         front.z = glm.sin(glm.radians(self.yaw)) * glm.cos(glm.radians(self.pitch))
         self.front = glm.normalize(front)
+
+    def update(self):
+        view_proj_data = self.get_view_matrix() + self.get_projection_matrix()
+        self.device.queue.write_buffer(self.uniform_buffer, 0, view_proj_data)
+
+    def _create_uniform_buffer(self) -> wgpu.GPUBuffer:
+        return self.device.create_buffer(
+            label="CAMERA_UNIFORM_BUFFER",
+            size=(4 * 4 * 4) * 2,  # 2x 4x4 matrices (view + proj)
+            usage=wgpu.BufferUsage.UNIFORM | wgpu.BufferUsage.COPY_DST,
+        )
+    
+    def _create_bind_group(self) -> wgpu.GPUBindGroup:
+        return self.device.create_bind_group(
+            label="CAMERA_BIND_GROUP",
+            layout=self.renderer.global_bgl,
+            entries=[
+                wgpu.BindGroupEntry(
+                    binding=0,
+                    resource=wgpu.BufferBinding(buffer=self.uniform_buffer),
+                ),
+            ],
+        )
